df6c4fa037996d8d63b886b8610440b14c83fb4f
sanjiv.chadhary <sanjiv.chaudhary@yipl.com.np>
2022-02-06 01:06:46 +0545

    tailwind jit congfig

---
+++ contact-app/src/App.js
@@ -0,0 +1,10 @@
+import "./styles/output.css"
+function App() {
+ return (
+   <div class=" text-[50px] border-green-600 border-b p-6 m-4 rounded font-bold text-center">
+     Hello World
+   </div>
+ );
+}
+
+export default App;
---
+++ contact-app/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mode: "jit",
+    content: [   
+        './public/**/*.html',    './src/**/*.{js,jsx,ts,tsx,vue}', 
+     ],
+    theme: {},
+};
